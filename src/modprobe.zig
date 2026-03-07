8f99c67ad865f00b608134f57ec2f1dde9ed767d
Jared Baur <jaredbaur@fastmail.com>
2026-03-07 09:28:08 -0800

    modprobe: skip flags passed by kernel

--- src/modprobe.zig
+++ src/modprobe.zig
@@ -13,6 +13,12 @@ pub fn main(args: *std.process.ArgIterator) !void {
     defer kmod.deinit();
 
     while (args.next()) |arg| {
+        // Ignore flags passed by kernel
+        // https://github.com/torvalds/linux/blob/4ae12d8bd9a830799db335ee661d6cbc6597f838/kernel/module/kmod.c#L92
+        if (std.mem.eql(u8, arg, "-q") or std.mem.eql(u8, arg, "--")) {
+            continue;
+        }
+
         kmod.modprobe(arg) catch |err| {
             log.err("module load for '{s}' failed: {}", .{ arg, err });
         };
