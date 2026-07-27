a5d65420957ef871eba39df0988265cf8b5ae587
Jared Baur <jaredbaur@fastmail.com>
2026-07-27 14:52:22 -0700

    Test that we don't crash if loading a module that is not available

--- testing/tests/kernel-modules.nix
+++ testing/tests/kernel-modules.nix
@@ -1,18 +1,34 @@
 { config, ... }: {
   name = "mixos-kernel-modules";
 
-  mixos.nodes.machine = { config, pkgs, ... }: {
-    # TODO(jared): formalize this as module option(s)
-    etc."modprobe.d/mixos.conf".source = pkgs.writeText "modprobe-mixos.conf" ''
-      options nvme-tcp wq_unbound=Y
-    '';
+  mixos.nodes.machine =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      # TODO(jared): formalize this as module option(s)
+      etc."modprobe.d/mixos.conf".source = pkgs.writeText "modprobe-mixos.conf" ''
+        options nvme-tcp wq_unbound=Y
+      '';
 
-    boot.extraModulePackages = [ config.boot.kernelPackages.jool ];
-    boot.kernelModules = [
-      "nvme-tcp"
-      "jool"
-    ];
-  };
+      boot.extraModulePackages = [ config.boot.kernelPackages.jool ];
+      boot.kernelModules = [
+        "nvme-tcp"
+        "jool"
+
+        # "i2c" is a kernel module that nixpkgs builds into kernel, so we test
+        # here that we can gracefully skip over drivers that are not built as
+        # modules, but requested to be loaded as such at runtime. See
+        # https://github.com/nixos/nixpkgs/blob/5f0dc9a0153be4a724176e0b436e06a7e1bfe4fe/pkgs/os-specific/linux/kernel/common-config.nix#L192
+        "i2c"
+      ];
+
+      # Ensure that our comment above is accurate.
+      boot.requiredKernelConfig.I2C = lib.kernel.yes;
+    };
 
   testScript = ''
     import mixos
@@ -26,6 +42,9 @@
 
         # out-of-tree module loads successfully
         machine.succeed("lsmod | grep jool")
+
+        # builtin driver fails to load
+        machine.succeed("dmesg | grep 'failed to load module i2c'")
     except: raise
     finally:
         machine.shutdown()
