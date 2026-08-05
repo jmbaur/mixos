b07e5eaec03c320e8e2bad14650f6c65e2fb8691
Jared Baur <jaredbaur@fastmail.com>
2026-08-04 22:28:17 -0700

    Version 1.8.1

--- package.nix
+++ package.nix
@@ -28,7 +28,7 @@ stdenvNoCC.mkDerivation (
   in
   {
     pname = lib.concatStringsSep "-" ([ "mixos" ] ++ lib.optional buildTools "buildtools");
-    version = "1.8.0";
+    version = "1.8.1";
 
     src = lib.fileset.toSource {
       root = ./.;
