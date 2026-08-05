5138ad6e32629232b6fca9a81315295310f4ae42
Jared Baur <jaredbaur@fastmail.com>
2026-08-04 22:54:51 -0700

    Fix libmnl buffer alignment

--- src/netlink.zig
+++ src/netlink.zig
@@ -9,22 +9,24 @@ const C = @cImport({
 });
 
 pub fn setInterfaceState(ifname: [:0]const u8, state: enum { up, down }) !void {
-    var buf: [std.heap.page_size_min]u8 = undefined;
+    var buf: [std.heap.page_size_min]u8 align(C.MNL_ALIGNTO) = undefined;
 
     const seq: c_uint = @intCast(C.time(null));
 
-    const nlh = C.mnl_nlmsg_put_header(&buf);
-    nlh[0].nlmsg_type = C.RTM_NEWLINK;
-    nlh[0].nlmsg_flags = C.NLM_F_REQUEST | C.NLM_F_ACK;
-    nlh[0].nlmsg_seq = seq;
+    const nlh = @as(?*C.nlmsghdr, @ptrCast(@alignCast(C.mnl_nlmsg_put_header(
+        @ptrCast(&buf),
+    )))) orelse return error.MnlPutHeader;
+    nlh.nlmsg_type = C.RTM_NEWLINK;
+    nlh.nlmsg_flags = C.NLM_F_REQUEST | C.NLM_F_ACK;
+    nlh.nlmsg_seq = seq;
 
-    var ifm: [*c]C.ifinfomsg = @ptrCast(@alignCast(C.mnl_nlmsg_put_extra_header(
+    var ifm = @as(?*C.ifinfomsg, @ptrCast(@alignCast(C.mnl_nlmsg_put_extra_header(
         nlh,
         @sizeOf(C.ifinfomsg),
-    ) orelse return error.MnlPutExtraHeader));
-    ifm[0].ifi_family = C.AF_UNSPEC;
-    ifm[0].ifi_change = C.IFF_UP;
-    ifm[0].ifi_flags = switch (state) {
+    )))) orelse return error.MnlPutExtraHeader;
+    ifm.ifi_family = C.AF_UNSPEC;
+    ifm.ifi_change = C.IFF_UP;
+    ifm.ifi_flags = switch (state) {
         .up => C.IFF_UP,
         .down => 0 & ~C.IFF_UP,
     };
@@ -40,16 +42,16 @@ pub fn setInterfaceState(ifname: [:0]const u8, state: enum { up, down }) !void {
 
     const portid = C.mnl_socket_get_portid(nl);
 
-    if (C.mnl_socket_sendto(nl, nlh, nlh[0].nlmsg_len) < 0) {
+    if (C.mnl_socket_sendto(nl, nlh, nlh.nlmsg_len) < 0) {
         return error.MnlSocketSendto;
     }
 
-    const ret = C.mnl_socket_recvfrom(nl, &buf, buf.len);
+    const ret = C.mnl_socket_recvfrom(nl, @ptrCast(&buf), buf.len);
     if (ret == -1) {
         return error.MnlSocketRecvfrom;
     }
 
-    if (C.mnl_cb_run(&buf, @intCast(ret), seq, portid, null, null) == -1) {
+    if (C.mnl_cb_run(@ptrCast(&buf), @intCast(ret), seq, portid, null, null) == -1) {
         return error.MnlCbRun;
     }
 }
